6958cff7d5f047f3b09d161b01b9f5eada66c89e
btamas97 <tamas.barath@gmail.com>
2019-11-12 22:23:21 +0100

    - added test

--- app/src/test/java/com/example/dreamer/etelbarserver/ExampleUnitTest.java
+++ app/src/test/java/com/example/dreamer/etelbarserver/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.dreamer.etelbarserver;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,19 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private Order order;
+
+    @Before
+    public void init(){
+        order = new Order();
+    }
+
+    @Test
+    public void setStatus_isCorrect(){
+        order.setStatus("paid");
+        assertEquals("paid",order.getStatus());
+    }
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
